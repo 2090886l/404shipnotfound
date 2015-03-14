8fa8037174ef88f00745eb22338df6ceda76bacb
2090886l <ivailolafchiev@yahoo.com>
2015-03-14 16:32:06 +0000

    added the models

--- shipnotfound/_404shipnotfound/admin.py
+++ shipnotfound/_404shipnotfound/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from _404shipnotfound.models import UserProfile, Game
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Game)
--- shipnotfound/_404shipnotfound/models.py
+++ shipnotfound/_404shipnotfound/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    high_score = models.IntegerField(default=0)
+    picture = models.ImageField(upload_to='profile_images', blank=True) #still working on picture
+    def __unicode__(self):
+        return unicode(self.user)
+        
+        
+class Game(models.Model):
+    user = models.ForeignKey(User)
+    score = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)  
+    def __unicode__(self):
+        return unicode(self.date)
