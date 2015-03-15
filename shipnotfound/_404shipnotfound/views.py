90b8d7840505d0121a1cc65900cc0e6612907bd2
2090886l <ivailolafchiev@yahoo.com>
2015-03-15 19:54:44 +0000

    added some more functionality to home page

--- shipnotfound/_404shipnotfound/views.py
+++ shipnotfound/_404shipnotfound/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from _404shipnotfound.models import Game
+from _404shipnotfound.models import Game, UserProfile
 
 
 
@@ -7,12 +7,13 @@ def index(request):
 
 
 
-    return render(request, 'app/index.html', {"games" : list})
+    return render(request, 'app/index.html')
     
 def home(request):
 
 
     list = Game.objects.order_by('-score')[:10]
+    userprofile = UserProfile.objects.get(user = request.user)
 
 
-    return render(request, 'app/home.html', {"games" : list})
+    return render(request, 'app/home.html', {"games" : list, "profile" : userprofile})
