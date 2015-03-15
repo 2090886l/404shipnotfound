7549845ea0557afa00508d0b853f061728db85d7
2090886l <ivailolafchiev@yahoo.com>
2015-03-15 21:18:23 +0000

    Fixed crash when user isn't logged in

--- shipnotfound/_404shipnotfound/views.py
+++ shipnotfound/_404shipnotfound/views.py
@@ -10,10 +10,10 @@ def index(request):
     return render(request, 'app/index.html')
     
 def home(request):
-
-
     list = Game.objects.order_by('-score')[:10]
-    userprofile = UserProfile.objects.get(user = request.user)
-
+    if request.user.is_authenticated():
+        userprofile = UserProfile.objects.get(user = request.user)
+    else:
+        userprofile = ""
 
     return render(request, 'app/home.html', {"games" : list, "profile" : userprofile})
