700107778ce7ca6dc26f5484f817ba03885a2a41
Greg <GregisGinger2@googlemail.com>
2015-03-21 11:09:08 +0000

    test cases added

--- shipnotfound/_404shipnotfound/models.py
+++ shipnotfound/_404shipnotfound/models.py
@@ -5,14 +5,25 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)
     high_score = models.IntegerField(default=0)
     picture = models.ImageField(upload_to='profile_images', blank=True, default="static/brightkite.png")
+
+    def save(self, *args, **kwargs):
+        if self.high_score < 0:
+            self.high_score = 0
+        super(UserProfile, self).save(*args, **kwargs)
+
     def __unicode__(self):
         return unicode(self.user.username)
-        
-        
+
 class Game(models.Model):
     user = models.ForeignKey(User)
     score = models.IntegerField(default=0)
     date = models.DateTimeField(auto_now_add=True)
     win = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.score < 0:
+            self.score = 0
+        super(Game, self).save(*args, **kwargs)
+
     def __unicode__(self):
         return unicode(self.date)
--- shipnotfound/_404shipnotfound/tests.py
+++ shipnotfound/_404shipnotfound/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from _404shipnotfound.models import Game, UserProfile
 
-# Create your tests here.
+class GameMethodTests(TestCase):
+
+    # ensure_scores_are_positive should results True for games where scores are zero or positive
+    def test_ensure_scores_are_positive(self):
+        game = Game(user = User.objects.create_user(username='test', email='test@test.com', password='top_secret'), score = -1)
+        game.save()
+        self.assertEqual((game.score >= 0), True)
+
+class UserProfileMethodTests(TestCase):
+
+    # ensure_high_scores_are_positive should results True for userProfiles where high scores are zero or positive
+    def test_ensure_high_scores_are_positive(self):
+        u = UserProfile(user = User.objects.create_user(username='test', email='test@test.com', password='top_secret'), high_score = -1)
+        u.save()
+        self.assertEqual((u.high_score >= 0), True)
+
+
+class HomeViewTests(TestCase):
+
+    # test view opens
+    def test_home_view_works(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+class IndexViewTests(TestCase):
+
+    # test view opens
+    def test_index_view_works(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+class PlayViewTests(TestCase):
+
+    # test view opens
+    def test_play_view_works(self):
+        response = self.client.get(reverse('play'))
+        self.assertEqual(response.status_code, 200)
