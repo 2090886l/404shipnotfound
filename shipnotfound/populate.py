6a83f07a11bb50d2e1fe7b981e987eefba0d34ea
2090886l <ivailolafchiev@yahoo.com>
2015-03-27 18:24:07 +0000

    changes to pop script

--- shipnotfound/populate.py
+++ shipnotfound/populate.py
@@ -12,11 +12,11 @@ from django.contrib.auth.models import User
 def populate():
     # add users and games
 
-    user1=add_player("George", "clooney", "george@gmail.com")
-    user2=add_player("test", "test", "test@test.com")
-    user3=add_player("Sam", "sammy123", "Sam@gmail.com")
-    user4=add_player("Bob", "thebuilder", "Bobby@yahoo.com")
-    user5=add_player("TheDestroyer", "42069", "destroy@kill.com")
+    user1=add_player("George", "clooney", "george@gmail.com", 69)
+    user2=add_player("test", "test", "test@test.com", 68)
+    user3=add_player("Sam", "sammy123", "Sam@gmail.com", 29)
+    user4=add_player("Bob", "thebuilder", "Bobby@yahoo.com", 0)
+    user5=add_player("TheDestroyer", "42069", "destroy@kill.com", 12)
 
     add_game(user1, 69, True)
     add_game(user3, 29, True)
@@ -31,14 +31,14 @@ def add_game(user, score, win):
     g = Game.objects.get_or_create(user = user, score = score)[0]
     return g
 
-def add_player(name, pw, email):
+def add_player(name, pw, email, highscore):
     try:
         user = User.objects.get(username=name)
     except:
         user = User(username=name, email=email)
         user.set_password(pw)
         user.save()
-        profile = UserProfile(user=user, picture = "/static/img/default.png")
+        profile = UserProfile(user=user, picture = "/static/img/default.png", highscore = highscore)
         profile.save()
     return user
 
